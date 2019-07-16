82b423c7dc30a5ec5a2370339f3c11c87c8d1e12
TangKaifei <tangkaifei@smyfinancial.com>
2019-07-16 20:58:05 +0800

    异常

---
+++ chapter3-b/src/main/java/com/smy/chapter3/exception/AgeDeleteException.java
@@ -0,0 +1,24 @@
+package com.smy.chapter3.exception;
+
+public class AgeDeleteException extends RuntimeException {
+
+    public AgeDeleteException() {
+        super();
+    }
+
+    public AgeDeleteException(String message) {
+        super(message);
+    }
+
+    public AgeDeleteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AgeDeleteException(Throwable cause) {
+        super(cause);
+    }
+
+    protected AgeDeleteException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
